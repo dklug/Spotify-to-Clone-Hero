0498eed8fa4e2fc5cadce41092a7d7c05ddd10e0
Duncan <dklug@ucmerced.edu>
2018-11-20 12:56:05 -0800

    Added folder selector, Chorus comparator

--- renderer.js
+++ renderer.js
@@ -3,6 +3,8 @@
 // All of the Node.js APIs are available in this process.
 
 var request = require('request'); // "Request" library
+const {dialog} = require('electron').remote;
+var fs = require('fs');
 
 var client_id = 'YOUR-CLIENTID-HERE'; // Your client id
 var client_secret = 'YOUR-CLIENTSECRET-HERE'; // Your secret
@@ -18,6 +20,12 @@ var authOptions = {
   },
   json: true
 };
+
+// Set up folder select button
+let selectbutton = document.querySelector('.selectfolder');
+selectbutton.addEventListener('click', function (error) {
+  selectCloneHeroFolder();
+});
 
 // Query username when button pressed
 let buttonquery = document.querySelector('.test');
@@ -81,11 +89,88 @@ function getTracks(token,username,playlistid){
     json: true
   };
   // console.log(options.url);
+  let trackString = "";
   request.get(options, function (error, response, body) {
-    console.log(body);
+    // console.log(body);
     for (let item in body.items){
-      track = body.items[item].track;
-      console.log(track.name);
+      let track = body.items[item].track;
+      // console.log(track);
+      trackString+=track.name+"<br>"
+
+      let album = track.album.name;
+      let artist = track.artists[0].name;
+      // console.log(album+", "+artist);
+
+      checkChorus(`name="`+track.name+`" artist="`+artist+`" album="`+album+`"`);
+    }
+    let trackList = document.querySelector('.tracks');
+    trackList.innerHTML=trackString;
+  });
+  
+}
+
+function checkChorus(queryString){
+  console.log(queryString);
+  var options = {
+    url: 'https://chorus.fightthe.pw/api/search?query='+queryString,
+    json: true
+  };
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      // console.log(body);
+      if (body.songs.length==0){
+        console.log("Song not found");
+      }
+      else{
+        let bestMatch = body.songs[0];
+        console.log(bestMatch);
+        saveFile(bestMatch.directLinks.archive);
+      }
     }
   });
+}
+
+function saveFile(link){
+  console.log("saveFile called with link: " +link);
+  var options = {
+    url: link
+  };
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log(response.headers);
+      let filename = response.headers['content-disposition'].split("filename*=UTF-8''")[1];
+      let chpath = document.querySelector('.chpath').value;
+      let filepath = chpath+"/Songs/"+filename;
+      console.log(filepath);
+      // Would check beforehand for existing file but Google Drive doesn't seem to support HEAD requests
+      // if (fs.existsSync(filepath)){
+        // console.log("File already exists");
+      // }
+      // else{
+        console.log("Writing file");
+        try {
+          // fs.writeFileSync(filepath,body);
+          let writeStream = fs.createWriteStream(filepath);
+          writeStream.write(body);
+          writeStream.end();
+        }
+        catch(error){
+          console.log("file write failed:");
+          console.log(error);
+        }
+      // }
+    }
+  });
+}
+
+function selectCloneHeroFolder(){
+  let cloneHeroPath = dialog.showOpenDialog({
+    properties: ['openDirectory']
+  })
+
+  console.log(cloneHeroPath[0]);
+  let chpath = document.querySelector('.chpath');
+  chpath.value = cloneHeroPath[0];
+  // chpath.innerHTML=cloneHeroPath[0];
+  // return cloneHeroPath[0];
 }
